08c8e4e94c904b38929d832bad1e95f7b9a810de
Acrylic125 <acrylic125email@gmail.com>
2025-12-02 14:15:00 +0800

    a

--- 2/a.py
+++ 2/a.py
@@ -1,22 +1,37 @@
+import re
 
 
+def has_pattern(s):
+    return re.match(r"^(.+)\1$", s)
+    # return re.match(r"^(.+)(\1)+$", s)
+    # return re.match(r"^(.+?)(?:\\1)+$", s)
+    # cur_pattern = ""
+    # for c in s:
+    #     if cur_pattern
 
-dial = 50
-full_dial = 100
-num_of_0 = 0
+# print(has_pattern("121212"))
+# print(has_pattern("12121"))
+
+# with open("2/test.txt", "r") as f:
 with open("2/input.txt", "r") as f:
-# with open("1/test.txt", "r") as f:
-    lines = f.readlines()
-    for l in lines:
-        if l.startswith("L"):
-            rotation = int(l.replace("L", ''))
-            dial = (dial - rotation) % full_dial
-        if l.startswith("R"):
-            rotation = int(l.replace("R", ''))
-            dial = (dial + rotation) % full_dial
-        if dial == 0:
-            num_of_0 += 1
+    line = f.readline()
+    line = line.replace("\n", "")
+    all_ranges = line.split(",")
+    invalid_ids_sum = 0
+    for ranges in all_ranges:
+        split = ranges.split("-")
+        if len(split) != 2:
+            continue
+        _l, _h = split
+        l = int(_l)
+        h = int(_h)
+        # print(h-l)
+        for i in range(l, h+1):
+            if has_pattern(f"{i}"):
+                print(f"{l} {h+1}: {i}")
+                invalid_ids_sum += i
+    print(invalid_ids_sum)
+                
 
-print(num_of_0)
-
+        
 
