719aec58e1de321dd46a398c20001f0c0414826d
Acrylic125 <acrylic125email@gmail.com>
2025-12-05 13:29:44 +0800

    b

--- 5/b.py
+++ 5/b.py
@@ -1,65 +1,31 @@
 
-def replace_char_at_pos(original_string, position, new_char):
-  if not (0 <= position < len(original_string)):
-    raise IndexError("Position out of bounds for the given string.")
-  
-  return original_string[:position] + new_char + original_string[position+1:]
+# with open("5/test.txt", "r") as f:
+with open("5/input.txt", "r") as f:
+    fresh_ranges = []
 
-# with open("4/test.txt", "r") as f:
-with open("4/input.txt", "r") as f:
     lines = f.readlines()
-
-    grid = []
-    marked = []
     for l in lines:
-        grid.append(l)
-        marked.append([0] * len(l))
-    print(f"{len(grid)} {len(grid[0])}")
-
-    dirs = [
-            [1, 0],
-            [0, 1],
-            [1, 1],
-            [1, -1],
-            [-1, 0],
-            [0, -1],
-            [-1, -1],
-            [-1, 1],
-    ]
-
-    prev_tally = 0
-    while True: 
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] != '@':
-                    continue
-                # number_of_rolls = 0
-                rolls = []
-                has_forklift = False
-                for (dx, dy) in dirs:
-                    nx = i + dx
-                    ny = j + dy
-                    if nx < 0 or nx >= len(grid):
-                        continue
-                    if ny < 0 or ny >= len(grid[0]):
-                        continue
-                    if grid[nx][ny] == '.':
-                        has_forklift = True
-                    if grid[nx][ny] == '@':
-                        rolls.append((nx, ny))
-                # print(f"{i} {j} {rolls}")
-                if len(rolls) < 4 and has_forklift:
-                    marked[i][j] = 1
-
-        tally = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                tally += marked[i][j]
-                if marked[i][j] == 1:
-                    grid[i] = replace_char_at_pos(grid[i], j, '.')
-        print(tally)
-        if prev_tally == tally:
-            print(tally)
+        l = l.replace("\n", "")
+        if l == "":
             break
-        prev_tally = tally
+        _min, _max = l.split("-")
+        _min, _max = int(_min), int(_max)
+        if _max < _min:
+            raise ValueError("max smaller than min")
+        fresh_ranges.append((_min, _max))
+
+    fresh_ranges.sort(key=lambda a: a[0])
+    last_highest = fresh_ranges[0][0] - 1
+    tally = 0
+    for (_min, _max) in fresh_ranges:
+        modified_min = max(_min, last_highest + 1)
+        if last_highest > _max:
+            continue
+        delta = _max - modified_min + 1
+        print(modified_min, _max)
+        tally += delta
+        last_highest = _max
+    print(tally)
+     
+        
 
